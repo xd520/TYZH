87de8024f8809fdb34f8ab4443ee6bfb055b05dc
xd520 <327031193@qq.com>
2015-06-17 11:26:22 +0800

    Initial Commit

---
+++ UnifiedAccount/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UnifiedAccount
+//
+//  Created by mac on 15/6/17.
+//  Copyright (c) 2015年 ApexSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
